53e4783641b740d9e5969eea68876408c092d50b
n.athekame <n.athekame@gmail.com>
2020-04-10 08:47:02 +0100

    secondEdit

--- src/estimator.js
+++ src/estimator.js
@@ -1,6 +1,8 @@
 const covid19ImpactEstimator = (data) => {
 // task 1
-  const currentlyInfectedImpact = data.reportedCases * 10;
+  const { reportedCases } = data;
+
+  const currentlyInfectedImpact = reportedCases * 10;
   const infectionsByRequestedTimeImpact = currentlyInfectedImpact * 512;
 
   const dataToBeReturned = {};
@@ -10,7 +12,7 @@ const covid19ImpactEstimator = (data) => {
   dataToBeReturned.impact.currentlyInfected = currentlyInfectedImpact;
   dataToBeReturned.impact.infectionsByRequestedTime = infectionsByRequestedTimeImpact;
 
-  const currentlyInfectedSevere = data.reportedCases * 50;
+  const currentlyInfectedSevere = reportedCases * 50;
   const infectionsByRequestedTimeSevere = currentlyInfectedSevere * 512;
 
   dataToBeReturned.severeImpact.currentlyInfected = currentlyInfectedSevere;
