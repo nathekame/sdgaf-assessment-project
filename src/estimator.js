cb231703cbb0db0649defbc1471671e94f733454
n.athekame <n.athekame@gmail.com>
2020-04-10 10:11:02 +0100

    fouthCommit

--- src/estimator.js
+++ src/estimator.js
@@ -1,9 +1,22 @@
+import periodUtility from './periodUtility';
+
+
 const covid19ImpactEstimator = (data) => {
 // task 1
-  const { reportedCases } = data;
+  const { periodType, timeToElapse, reportedCases } = data;
+
+
+  const periodInDays = periodUtility(periodType, timeToElapse);
+
+  const periodFactor = periodInDays / 3;
+
+  const periodFactorTrunc = Math.trunc(periodFactor);
+
+  const x = 2 ** periodFactorTrunc;
+
 
   const currentlyInfectedImpact = reportedCases * 10;
-  const infectionsByRequestedTimeImpact = currentlyInfectedImpact * 512;
+  const infectionsByRequestedTimeImpact = currentlyInfectedImpact * x;
 
   const dataToBeReturned = {
     data: {},
@@ -17,7 +30,7 @@ const covid19ImpactEstimator = (data) => {
   dataToBeReturned.impact.infectionsByRequestedTime = infectionsByRequestedTimeImpact;
 
   const currentlyInfectedSevere = reportedCases * 50;
-  const infectionsByRequestedTimeSevere = currentlyInfectedSevere * 512;
+  const infectionsByRequestedTimeSevere = currentlyInfectedSevere * x;
 
   dataToBeReturned.severeImpact.currentlyInfected = currentlyInfectedSevere;
   dataToBeReturned.severeImpact.infectionsByRequestedTime = infectionsByRequestedTimeSevere;
