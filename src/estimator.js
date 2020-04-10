d091459af07055d3d543b51c9afe14cf061d1365
n.athekame <n.athekame@gmail.com>
2020-04-10 12:30:16 +0100

    secondChallenge

--- src/estimator.js
+++ src/estimator.js
@@ -12,8 +12,16 @@ const periodUtility = (periodType, periodCount) => {
 };
 
 const covid19ImpactEstimator = (data) => {
-// task 1
-  const { periodType, timeToElapse, reportedCases } = data;
+  /* first challenge */
+
+  const {
+
+    periodType,
+    timeToElapse,
+    reportedCases,
+    totalHospitalBeds
+
+  } = data;
 
 
   const periodInDays = periodUtility(periodType, timeToElapse);
@@ -24,25 +32,52 @@ const covid19ImpactEstimator = (data) => {
 
   const x = 2 ** periodFactorTrunc;
 
-  const currentlyInfectedImpact = reportedCases * 10;
-  const infectionsByRequestedTimeImpact = currentlyInfectedImpact * x;
-
   const dataToBeReturned = {
     data: {},
     impact: {},
     severeImpact: {}
   };
 
+  // data
   dataToBeReturned.data = data;
+
+  // impact(first challenge)
+  const currentlyInfectedImpact = reportedCases * 10;
+  const infectionsByRequestedTimeImpact = currentlyInfectedImpact * x;
 
   dataToBeReturned.impact.currentlyInfected = currentlyInfectedImpact;
   dataToBeReturned.impact.infectionsByRequestedTime = infectionsByRequestedTimeImpact;
 
+
+  // severe impact (first challenge)
   const currentlyInfectedSevere = reportedCases * 50;
   const infectionsByRequestedTimeSevere = currentlyInfectedSevere * x;
 
   dataToBeReturned.severeImpact.currentlyInfected = currentlyInfectedSevere;
   dataToBeReturned.severeImpact.infectionsByRequestedTime = infectionsByRequestedTimeSevere;
+
+  /* end of first challenge */
+
+  /* Second challenge */
+
+  const { impact, severeImpact } = dataToBeReturned;
+
+  const averageBedsOccupied = (totalHospitalBeds / 100) * 65;
+  const averageAvialableBeds = totalHospitalBeds - averageBedsOccupied;
+
+
+  // impact (second challenge)
+  const severeCasesByRequestedTimeImpact = (impact.infectionsByRequestedTime / 100) * 15;
+  const hospitalBedsByRequestedTimeImpact = averageAvialableBeds - severeCasesByRequestedTimeImpact;
+
+  dataToBeReturned.impact.hospitalBedsByRequestedTime = hospitalBedsByRequestedTimeImpact;
+
+
+  // severe impact(second challenge)
+  const severeCasesByRequestedTimeSevere = (severeImpact.infectionsByRequestedTime / 100) * 15;
+  const hospitalBedsByRequestedTimeSevere = averageAvialableBeds - severeCasesByRequestedTimeSevere;
+
+  dataToBeReturned.severeImpact.hospitalBedsByRequestedTime = hospitalBedsByRequestedTimeSevere;
 
 
   return dataToBeReturned;
