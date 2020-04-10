56832c39b4aa8e4369814640e5b95f562bc205ea
n.athekame <n.athekame@gmail.com>
2020-04-10 08:17:26 +0100

    firstCommit

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,23 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+// task 1
+  const currentlyInfectedImpact = data.reportedCases * 10;
+  const infectionsByRequestedTimeImpact = currentlyInfectedImpact * 512;
+
+  const dataToBeReturned = {};
+
+  dataToBeReturned.data = data;
+
+  dataToBeReturned.impact.currentlyInfected = currentlyInfectedImpact;
+  dataToBeReturned.impact.infectionsByRequestedTime = infectionsByRequestedTimeImpact;
+
+  const currentlyInfectedSevere = data.reportedCases * 50;
+  const infectionsByRequestedTimeSevere = currentlyInfectedSevere * 512;
+
+  dataToBeReturned.severeImpact.currentlyInfected = currentlyInfectedSevere;
+  dataToBeReturned.severeImpact.infectionsByRequestedTime = infectionsByRequestedTimeSevere;
+
+
+  return dataToBeReturned;
+};
 
 export default covid19ImpactEstimator;
