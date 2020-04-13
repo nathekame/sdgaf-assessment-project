1fbb2f4b11b69c35fcdc0d2c29dd8a0503c02077
n.athekame <n.athekame@gmail.com>
2020-04-13 02:11:21 +0100

    apiAssessment8edit

---
+++ app.js
@@ -0,0 +1,33 @@
+
+// import express from 'express';
+// import bodyParser from 'body-parser';
+// // import xmlMiddleware from 'xml-express-middleware';
+// import fs from 'fs';
+// import morgan from 'morgan';
+// import path from 'path';
+// import routes from './src/routes/api';
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const xmlMiddleware = require('xml-express-middleware').xml;
+const fs = require('fs');
+const morgan = require('morgan');
+const path = require('path');
+const routes = require('./src/routes/api');
+
+const app = express();
+
+const PORT = process.env.PORT || 6000;
+
+const logStream = fs.createWriteStream(path.join(__dirname, 'covid19estimator.log'));
+
+app.use(morgan(':method  :url  :status  :response-time ms', { stream: logStream }));
+
+app.use(bodyParser.json());
+app.use(xmlMiddleware());
+
+app.use('/', routes);
+
+
+app.listen(PORT);
---
+++ src/routes/estimatorRoutes.js
@@ -0,0 +1,46 @@
+// import fs from 'fs';
+// import covid19ImpactEstimator from '../estimator';
+
+const fs = require('fs');
+
+const estimator = require('./estimator');
+// const covid19ImpactEstimator = require('../estimator');
+
+const estimatorPostJSON = async (req, res) => {
+  const dataToBeReturned = estimator(req.body);
+  res.set('Content-Type', 'application/json');
+  res.send(dataToBeReturned);
+};
+
+const estimatorPostXML = async (req, res) => {
+  const dataToBeReturned = estimator(req.body);
+  res.set('Content-Type', 'application/xml');
+  res.xml(dataToBeReturned);
+};
+
+
+const estimatorGetLogs = async (req, res) => {
+  fs.stat('./covid19estimator.log', (err, stats) => {
+    if (err) {
+      res.sendStatus(404);
+    }
+    if (stats.isFile) {
+      const readStream = fs.createReadStream('./covid19estimator.log');
+      readStream.pipe(res);
+      readStream.on('error', () => {
+        res.sendStatus(500);
+      });
+      res.on('error', () => {
+        res.sendStatus(500);
+      });
+    } else {
+      res.sendStatus(404);
+    }
+  });
+};
+
+module.exports = {
+  estimatorPostJSON,
+  estimatorPostXML,
+  estimatorGetLogs
+};
