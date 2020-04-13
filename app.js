9a42082d84bdabc79c374f2b2b4e012653b3945c
n.athekame <n.athekame@gmail.com>
2020-04-13 20:24:14 +0100

    apiAssessmentEdtitin

--- app.js
+++ app.js
@@ -19,16 +19,17 @@ const logStream = fs.createWriteStream(path.join(__dirname, 'covid19estimator.lo
 const checkResTime = (data) => {
   const d = data * 10;
   const a = Math.trunc(d);
+  const z = 'ms';
   if (a < 10) {
     const x = `0${a}`;
-    return x;
+    return x + z;
   }
   if (a > 99) {
     const x = a / 10;
     const y = Math.trunc(x);
-    return y;
+    return y + z;
   }
-  return a;
+  return a + z;
 };
 
 const dMorgan = morgan((tokens, req, res) => {
@@ -36,7 +37,7 @@ const dMorgan = morgan((tokens, req, res) => {
     tokens.method(req, res),
     tokens.url(req, res),
     tokens.status(req, res),
-    checkResTime(tokens['response-time'](req, res)), 'ms'
+    checkResTime(tokens['response-time'](req, res))
   ].join('  ');
 }, { stream: logStream });
 
